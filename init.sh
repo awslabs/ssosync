0ac6a0ec6f1bc8e46ded744f4fe9858f52bd34d8
Rafael Leonardo da Cruz <rafael.leonardo.cruz@outlook.com>
2020-11-18 17:30:50 -0300

    [PROD-1224] Add build process and deployment pipeline #2
    ## Why
    - As described on [SCD - Setup sync for users and groups](https://creditas.atlassian.net/browse/PROD-1224) we faced some problems with the synchronization process. IAM Team determined that it would run up to 2 hours, but the process with the original binary takes a several execution time (72 hours).
    - We implemented the changes from this [PR](https://github.com/awslabs/ssosync/pull/16) and containerized this app to run with ECS (Our initial idea was to use AWS Lambda, but this sync can take more than 15 minutes of execution time)
    - The cooldown time can be specified using a parameterStore variable
    
    ## What
    - Add a Docker file with a multi-stage build process that results in an executable image with 26Mbs 
    - Add a deployment pipeline to run with circle-ci
    - Implement the changes from this [PR](https://github.com/awslabs/ssosync/pull/16), and add the capability to getting `include-groups` from environment variables.
    
    ## Jira
    - https://creditas.atlassian.net/browse/PROD-1224

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+set -e
+
+echo $SSOSYNC_GOOGLE_CREDENTIALS_SECRET | base64 -d - > $SSOSYNC_GOOGLE_CREDENTIALS
+
+echo "ENVIRONMENT_NAME: ${ENV}"
+COMMAND=${1:-"init"}
+
+case "$COMMAND" in 
+  *)
+    while true; do
+      echo "Initializing sync process"
+      ./ssosync
+      echo "sync already finished"
+
+      echo "Waiting $(COOLDOWN_TIME) for next execution"
+      sleep $COOLDOWN_TIME
+    done
+    ;;
+esac
