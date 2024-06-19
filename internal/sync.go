8e6121d80433cd23fe753fa347131cb8e64de48d
Chris Pates <crpates@amazon.co.uk>
2024-06-19 10:26:00 +0100

    Update sync.go

--- internal/sync.go
+++ internal/sync.go
@@ -597,19 +597,20 @@ func (s *syncGSuite) getGoogleGroupsAndUsers(queryGroups string, queryUsers stri
 
 		// If we've not seen the user email address before add it to the list of unique users
 		// also, we need to deduplicate the list of members.
+		gUniqMembers := make(map[string]*admin.User)
                 for _, m := range membersUsers {
 			_, ok := gUniqUsers[m.PrimaryEmail]
 			if !ok {
 				gUniqUsers[m.PrimaryEmail] = gUserDetailCache[m.PrimaryEmail]
 			}
 
-			_, ok := gUniqMembers[m.PrimaryEmail]
+			_, ok = gUniqMembers[m.PrimaryEmail]
                         if !ok {
                                 gUniqMembers[m.PrimaryEmail] = gUserDetailCache[m.PrimaryEmail]
                         }
 		}
 
-	        gMembers := make([]*admin.User)
+	        gMembers := make([]*admin.User, 0)
 	        for _, member := range gUniqMembers {
                         gMembers = append(gMembers, member)
                 }
