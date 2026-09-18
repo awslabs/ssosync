1e51a1d81f6c618334cb7d077231bc324637c168
Chris Pates <ChrisPates@users.noreply.github.com>
2026-09-18 17:53:39 +0100

    Update Readme

--- cmd/root.go
+++ cmd/root.go
@@ -356,7 +356,6 @@ func getEncryptParam(parameterArn string) string {
 }
 
 func addFlags(_ *cobra.Command, cfg *config.Config) {
-	rootCmd.PersistentFlags().StringVarP(&cfg.GoogleCredentials, "google-admin", "a", config.DefaultGoogleCredentials, "path to find credentials file for Google Workspace")
 	rootCmd.PersistentFlags().BoolVarP(&cfg.Debug, "debug", "d", config.DefaultDebug, "enable verbose / debug logging")
 	rootCmd.PersistentFlags().StringVarP(&cfg.LogFormat, "log-format", "", config.DefaultLogFormat, "log format")
 	rootCmd.PersistentFlags().StringVarP(&cfg.LogLevel, "log-level", "", config.DefaultLogLevel, "log level")
